36b938562a3a4b2584d0dba0d65a5606d44f7507
AhabweAnnah <ahabweannah@gmail.com>
2021-11-13 14:57:59 +0300

    added date form

--- resources/views/pages/support_team/payments/create.blade.php
+++ resources/views/pages/support_team/payments/create.blade.php
@@ -4,7 +4,7 @@
 
     <div class="card">
         <div class="card-header header-elements-inline">
-            <h6 class="card-title">Create Payment</h6>
+            <h6 class="card-title">Set A deadline</h6>
             {!! Qs::getPanelOptions() !!}
         </div>
 
@@ -16,15 +16,15 @@
                         <div class="form-group row">
                             <label class="col-lg-3 col-form-label font-weight-semibold">Title <span class="text-danger">*</span></label>
                             <div class="col-lg-9">
-                                <input name="title" value="{{ old('title') }}" required type="text" class="form-control" placeholder="Eg. School Fees">
+                                <input name="title" value="{{ old('title') }}" required type="text" class="form-control" placeholder="Eg. ">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="my_class_id" class="col-lg-3 col-form-label font-weight-semibold">Class </label>
+                            <label for="my_class_id" class="col-lg-3 col-form-label font-weight-semibold">Course </label>
                             <div class="col-lg-9">
                                 <select class="form-control select-search" name="my_class_id" id="my_class_id">
-                                    <option value="">All Classes</option>
+                                    <option value="">All Courses</option>
                                     @foreach($my_classes as $c)
                                         <option {{ old('my_class_id') == $c->id ? 'selected' : '' }} value="{{ $c->id }}">{{ $c->name }}</option>
                                     @endforeach
@@ -32,22 +32,14 @@
                             </div>
                         </div>
 
+                        @csrf
                         <div class="form-group row">
-                            <label for="method" class="col-lg-3 col-form-label font-weight-semibold">Payment Method</label>
+                            <label class="col-lg-3 col-form-label font-weight-semibold">Set a deadline <span class="text-danger">*</span></label>
                             <div class="col-lg-9">
-                                <select class="form-control select" name="method" id="method">
-                                    <option selected value="Cash">Cash</option>
-                                    <option disabled value="Online">Online</option>
-                                </select>
+                                <input name="deadline" value="{{ old('dateline') }}" required type="date" class="form-control">
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="amount" class="col-lg-3 col-form-label font-weight-semibold">Amount (<del style="text-decoration-style: double">N</del>) <span class="text-danger">*</span></label>
-                            <div class="col-lg-9">
-                                <input class="form-control" value="{{ old('amount') }}" required name="amount" id="amount" type="number">
-                            </div>
-                        </div>
 
                         <div class="form-group row">
                             <label for="description" class="col-lg-3 col-form-label font-weight-semibold">Description</label>
@@ -57,7 +49,7 @@
                         </div>
 
                         <div class="text-right">
-                            <button type="submit" class="btn btn-primary">Submit form <i class="icon-paperplane ml-2"></i></button>
+                            <button type="submit" class="btn btn-primary btn-rounded">create deadline <i class="icon-paperplane ml-2"></i></button>
                         </div>
                     </form>
                 </div>
